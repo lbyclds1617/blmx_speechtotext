3b6a78dc410de2b15207b8ecb663b30c3c26b450
Andres Jr III R Torralba <a.r.torralba.iii@gmail.com>
2017-02-09 10:54:15 +0800

    Initial Commit

---
+++ src/main/java/net/tutorial/controllers/MainController.java
@@ -0,0 +1,102 @@
+package net.tutorial.controllers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import net.tutorial.utilities.SpeechToTextService;
+
+@WebServlet({ "/home", "" })
+@MultipartConfig
+public class MainController extends HttpServlet {
+
+	RequestDispatcher dispatcher;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		final Part filePart = req.getPart("file");
+		String filePath = saveFile(filePart);
+		
+		SpeechToTextService s2t = new SpeechToTextService();
+		
+		dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+		req.setAttribute("transcription", s2t.getTranscription(new File(filePath)));
+		dispatcher.forward(req, resp);
+	}
+
+	// File Management	
+	
+	private String saveFile(Part filePart) throws IOException {
+		final String fileName = getFileName(filePart);
+		OutputStream out = null;
+		InputStream filecontent = null;
+		String filePath = setDir("fileResource") + File.separator + fileName;
+
+		try {
+
+			out = new FileOutputStream(new File(filePath));
+
+			filecontent = filePart.getInputStream();
+			int read = 0;
+			final byte[] bytes = new byte[1024];
+
+			while ((read = filecontent.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			
+			return filePath;
+			
+		} catch (FileNotFoundException fne) {
+			fne.printStackTrace();
+			return "";
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+			if (filecontent != null) {
+				filecontent.close();
+			}
+		}
+
+	}
+
+	private String getFileName(final Part part) {
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+			}
+		}
+		return null;
+	}
+
+	private String setDir(String name) {
+		File dir = new File(name);
+		if (!dir.exists()) {
+			try {
+				dir.mkdir();
+			} catch (SecurityException se) {
+				se.printStackTrace();
+			}
+		}
+		return name;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Watson - Speech To Text</title>
+</head>
+<body>
+
+	
+	<legend>Convert Speech to Text</legend>
+	
+	<form method="POST" action="home" enctype="multipart/form-data">
+		<input type="file" name="file" accept=".wav">
+		<input type="submit" name="submit" value="Convert">
+	</form>
+	
+	<p><c:if test="${!empty transcription}">${transcription}</c:if></p>
+
+</body>
+</html>
